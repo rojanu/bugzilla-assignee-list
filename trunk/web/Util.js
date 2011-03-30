c8723d97d460d1f48ee7b303942dfc4d20aaabd9
aliustek@gmail.com <aliustek@gmail.com@91f67772-ad4f-b872-3215-f0b22f685d78>
2011-03-30 10:51:35 +0000

    Finished creating Assignee Structure, adding files

---
+++ trunk/web/Util.js
@@ -0,0 +1,28 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the AssigneeList Bugzilla Extension.
+ *
+ * The Initial Developer of the Original Code is Ali Ustek
+ * Portions created by the Initial Developer are Copyright (C) 2011 the
+ * Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Ali Ustek <aliustek@gmail.com>
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+function setAssigneeLabel () {
+  var mail = document.getElementById('assignee');
+  document.getElementById('name').value = mail.options[mail.selectedIndex].text;
+}
